9ab920e942c6d6c9cdbf3cc242107e0689da512a
Gagan <gagan20rajput@gmail.com>
2025-08-26 09:50:56 +0530

    fixed

--- src/main/java/com/algotutor/chatroom/controller/MessageController.java
+++ src/main/java/com/algotutor/chatroom/controller/MessageController.java
@@ -8,7 +8,6 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algotutor.chatroom.models.Message;
